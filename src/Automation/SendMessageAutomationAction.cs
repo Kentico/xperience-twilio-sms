a5e9fc2d97cff5db3f47931dc77846cb4f5c6515
Eric Dugre <ericd@kentico.com>
2022-11-16 12:29:31 -0500

    update SendMessageAutomationAction with new API, remove error localization

--- src/Automation/SendMessageAutomationAction.cs
+++ src/Automation/SendMessageAutomationAction.cs
@@ -1,11 +1,12 @@
 ﻿using CMS.ContactManagement;
 using CMS.Core;
 using CMS.EventLog;
-using CMS.Helpers;
 
 using Kentico.Xperience.Twilio.SMS.Services;
 
 using System;
+
+using Twilio.Rest.Api.V2010.Account;
 
 namespace Kentico.Xperience.Twilio.SMS
 {
@@ -20,25 +21,29 @@ namespace Kentico.Xperience.Twilio.SMS
             var message = GetResolvedParameter("Text", String.Empty);
             if (String.IsNullOrEmpty(message))
             {
-                LogMessage(EventType.ERROR, nameof(SendMessageAutomationAction), ResHelper.GetString("Kentico.Xperience.Twilio.SMS.Error.EmptyBody"), Contact);
+                LogMessage(EventType.ERROR, nameof(SendMessageAutomationAction), "The SMS text cannot be empty.", Contact);
                 return;
             }
 
             var recipientColumnName = GetResolvedParameter("Recipient", String.Empty);
             if (String.IsNullOrEmpty(recipientColumnName))
             {
-                LogMessage(EventType.ERROR, nameof(SendMessageAutomationAction), ResHelper.GetString("Kentico.Xperience.Twilio.SMS.Error.EmptyRecipient"), Contact);
+                LogMessage(EventType.ERROR, nameof(SendMessageAutomationAction), "No contact phone number field selected.", Contact);
                 return;
             }
 
             var recipientNumber = Contact.GetStringValue(recipientColumnName, String.Empty);
             if (String.IsNullOrEmpty(recipientNumber))
             {
-                LogMessage(EventType.ERROR, nameof(SendMessageAutomationAction), ResHelper.GetString("Kentico.Xperience.Twilio.SMS.Error.EmptyRecipient"), Contact);
+                LogMessage(EventType.ERROR, nameof(SendMessageAutomationAction), $"No phone number found in contact field '{recipientColumnName}.'", Contact);
                 return;
             }
 
-            Service.Resolve<ITwilioSmsClient>().SendMessageFromService(message, recipientNumber).ConfigureAwait(false);
+            var options = new CreateMessageOptions(recipientNumber) {
+                Body = message
+            };
+
+            Service.Resolve<ITwilioSmsClient>().SendMessageAsync(options).ConfigureAwait(false);
         }
     }
 }
